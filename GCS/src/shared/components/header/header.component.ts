843ac351d1b72becdb4804d6d0b227f477b11209
abhishek kandwal <34614893+abhishek-kandwal@users.noreply.github.com>
2019-08-28 12:21:47 +0530

    Add files via upload

--- GCS/src/shared/components/header/header.component.ts
+++ GCS/src/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserService } from '../../service/user.Service';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -11,20 +11,30 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class HeaderComponent {
   SearchResult = false;
   Data: any;
+  ResultNextIndex: any;
+  ResultprevIndex: any;
 
-  constructor(private Userservice: UserService , private domSanitizer:DomSanitizer) { }
+  constructor(private Userservice: UserService , private domSanitizer: DomSanitizer) { }
   @ViewChild('f', {static : false}) Searchbox: NgForm;
 
-  Onclick(){
-    this.Userservice.SearchResults(this.Searchbox.value[ 'Queryparam' ])
-    .subscribe(res => {
-      this.Data = res.items; // res.items[0].title
-      this.SearchResult = true;
-      console.log(this.Data);
-    });
-
-
+  OnclickSearch() {
+    // tslint:disable-next-line: max-line-length
+    const urlparams = 'q=' + this.Searchbox.value[ 'Queryparam' ] + '&num=5&cx=005314277313817109837%3Aznkkmtgnwyq&key=AIzaSyBJ-DvSxYno0czZGbsIRJqb2hULIopEPus' ;
+    this.Searchapihit(urlparams);
   }
+
+  prevSearch(){
+    // tslint:disable-next-line: max-line-length
+    const urlparams = 'q=' + this.Searchbox.value[ 'Queryparam' ] + '&num=5&start=' + this.ResultprevIndex + '&cx=005314277313817109837%3Aznkkmtgnwyq&key=AIzaSyBJ-DvSxYno0czZGbsIRJqb2hULIopEPus';
+    this.Searchapihit(urlparams);
+  }
+
+  nextSearch() {
+    // tslint:disable-next-line: max-line-length
+    const urlparams = 'q=' + this.Searchbox.value[ 'Queryparam' ] + '&num=5&start=' + this.ResultNextIndex + '&cx=005314277313817109837%3Aznkkmtgnwyq&key=AIzaSyBJ-DvSxYno0czZGbsIRJqb2hULIopEPus';
+    this.Searchapihit(urlparams);
+  }
+
 
   closeResults(){
     this.SearchResult = false;
@@ -34,4 +44,20 @@ export class HeaderComponent {
     return this.domSanitizer.bypassSecurityTrustHtml(htmlText);
   }
 
+
+  Searchapihit(Searchparam: string) {
+    this.Userservice.SearchResults(Searchparam)
+    .subscribe(res => {
+      this.Data = res.items; // res.items[0].title
+      this.ResultNextIndex = res.queries.nextPage[0].startIndex;
+
+      if (res.queries.previousPage) {
+        this.ResultprevIndex = res.queries.previousPage[0].startIndex;
+      }
+
+      this.SearchResult = true;
+      console.log(this.Data , this.ResultNextIndex);
+      console.log(res);
+    });
+  }
 }
