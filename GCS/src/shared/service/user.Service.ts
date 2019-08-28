edbf394f9c3b086ec746c1071c0e0f3934380b6d
abhishek kandwal <34614893+abhishek-kandwal@users.noreply.github.com>
2019-08-28 12:22:14 +0530

    Add files via upload

--- GCS/src/shared/service/user.Service.ts
+++ GCS/src/shared/service/user.Service.ts
@@ -7,11 +7,12 @@ import {googlesearchservice } from './google.serarch.service';
 
 export class UserService {
   // tslint:disable-next-line: max-line-length
-  private apiurl = 'https://www.googleapis.com/customsearch/v1?cx=005314277313817109837%3Aznkkmtgnwyq&key=AIzaSyBJ-DvSxYno0czZGbsIRJqb2hULIopEPus&q=';
-
+  private apiurl = 'https://www.googleapis.com/customsearch/v1?';
+  // tslint:disable-next-line: max-line-length
+  // private ApiUrl = 'https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json';
   constructor(private GoogleAPI: googlesearchservice) {}
 
   SearchResults(Queryparam: string) {
-    return this.GoogleAPI.getResults(this.apiurl +  Queryparam );
+    return this.GoogleAPI.getResults(this.apiurl + Queryparam);
   }
 }
